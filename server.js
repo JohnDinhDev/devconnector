cd8a541cf0c08b23be3254360551c6878bea7c82
John Dinh <johnduydinh@gmail.com>
2020-02-15 16:09:13 -0500

    add first json web token

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 // Connect Database
 connectDB();
 
-const app = express();
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
 	res.send("API is Running");

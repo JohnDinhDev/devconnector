ecd51c588d68ca643d78f543be2c97232d5aa52e
John Dinh <johnduydinh@gmail.com>
2020-02-15 15:19:18 -0500

    create user Schema

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 	res.send("API is Running");
 });
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);

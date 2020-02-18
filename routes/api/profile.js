23c0d1cac5544cbc6c3053bbdab05186936e9431
John Dinh <johnduydinh@gmail.com>
2020-02-18 05:57:52 -0500

    add endpoint to get github repo by username

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios").default;
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
@@ -326,6 +327,26 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 
 		res.json(profile);
 	} catch (error) {
+		console.error(error.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get("/github/:username", async (req, res) => {
+	try {
+		const options = {
+			url: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`,
+			method: "GET",
+		};
+		const response = await axios(options);
+		res.json(response.data);
+	} catch (error) {
+		if (error.response.status === 404) {
+			return res.status(404).json({ msg: "Github Profile Not Found" });
+		}
 		console.error(error.message);
 		res.status(500).send("Server Error");
 	}
